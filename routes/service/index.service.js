8ca30ff8f2cdb283e90d01d007e448d69891de04
Ukaye <itaukemeabasi@gmail.com>
2023-09-21 11:15:34 +0100

    prep for deployment

--- routes/service/index.service.js
+++ routes/service/index.service.js
@@ -2,6 +2,8 @@ const express = require('express'),
     router = express.Router(),
     enums = require('../../enums');
 
+router.get('/', (req, res) => res.send('You have reached the Payfeer service!'));
+
 router.get('/enums', (req, res) => res.send({
     status: 200,
     error: null,
