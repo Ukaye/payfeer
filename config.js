0ba3e8da4f1c6c0e2dbf7690e828ff9d0a2e6fed
Ukaye <itaukemeabasi@gmail.com>
2023-09-21 11:03:51 +0100

    db update

--- config.js
+++ config.js
@@ -1,11 +1,22 @@
+
 let config = {};
 
 config.staging = {
-    host: 'us-cdbr-east-04.cleardb.com',
+    host: '3.16.69.31',
     port: '3306',
-    user: 'baaebd40e9af8b',
-    password: '2c141c90',
-    database: 'heroku_aeff09f09516420',
+    user: 'finsoftapp',
+    password: 'LibertaDev@Finsoftapp2020',
+    database: 'payfeer',
+    charset: 'utf8mb4',
+    insecureAuth: true
+};
+
+config.production = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     charset: 'utf8mb4',
     insecureAuth: true
 };
